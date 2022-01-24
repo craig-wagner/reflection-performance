d1e998acbd5673e64af0307341d071c7c5fb24e1
Craig Wagner <craig.wagner@sterlingts.com>
2022-01-24 14:30:10 -0800

    Initial commit

---
+++ ReflectionPerformance/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ReflectionPerformance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Runner>();
+        }
+    }
+}
---
+++ ReflectionPerformance/Runner.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ReflectionPerformance
+{
+    public class Runner
+    {
+        private Calculator _calc = new Calculator();
+
+        [Benchmark]
+        public void DirectMethodCall() => _calc.Add(2, 3);
+
+        [Benchmark]
+        public void ReflectionMethodCall()
+        {
+            var methodInfo = _calc.GetType().GetMethod("Add");
+            methodInfo.Invoke(_calc, new object[] { 2, 3 });
+        }
+    }
+}
